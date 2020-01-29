c9163fa71e7763fbfef0e96a820979aa4ce3e49e
Hyde_Wick <hydekyle@hotmail.com>
2020-01-29 01:42:27 +0000

    SC

---
+++ GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public EnemyPiecesScriptableObject enemyPieces;
+
+    void Start()
+    {
+        SummonMonster(40);
+    }
+
+    void SummonMonster(int pieces)
+    {
+        Vector3 sumPos = transform.position;
+        float lastScaleX = 0f;
+        for (var x = 0; x <= pieces; x++)
+        {
+            GameObject basePiece = enemyPieces.pieces[Random.Range(0, enemyPieces.pieces.Count)].piece;
+            var go = Instantiate
+            (
+                basePiece,
+                sumPos + Vector3.right * (lastScaleX / 2 + basePiece.transform.lossyScale.x / 2), //Para que se peguen las piezas unas con otras
+                transform.rotation
+            );
+            sumPos = go.transform.position;
+            lastScaleX = go.transform.lossyScale.x;
+        }
+    }
+}
+
+[System.Serializable]
+public struct EnemyPiece
+{
+    public string name;
+    public int health;
+    public GameObject piece;
+}
